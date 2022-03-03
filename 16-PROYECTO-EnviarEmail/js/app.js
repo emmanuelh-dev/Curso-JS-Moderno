fd121ebce57a965684641b8bbdcb8bc87f0e8c2e
Emmanuel Hernandez <86961961+EmmaxPlay@users.noreply.github.com>
2022-03-02 22:25:10 -0600

    Validar los campos

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,10 +1,20 @@
 //variables
-
 const btnEnviar = document.querySelector('#enviar');
+
+//Variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje')
 
 eventListeners();
 function eventListeners(){
+    //Cuando la app arranca 
     document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    //Campos del formulario
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
 
 
@@ -12,4 +22,14 @@ function eventListeners(){
 function iniciarApp(){
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50')
+}
+
+
+//Valida el formulario
+function validarFormulario(e){
+    if(e.target.value.length > 0){
+        console.log("Sí hay algo");
+    }else{
+        e.target.style.borderColor = 'red'
+    }
 }
