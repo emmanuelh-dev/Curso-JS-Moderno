7a3d2bdcd02a848da908c3ed98420dab894e77ea
Emmanuel Hernandez <86961961+EmmaxPlay@users.noreply.github.com>
2022-03-05 14:37:18 -0600

    Mostrando los autos

---
+++ 17-PROYECTO-Buscador/js/app.js
@@ -0,0 +1,24 @@
+//Variables
+const resultado = document.querySelector("#resultado")
+
+
+//Eventos
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarAutos();
+} )
+
+//Funciones
+
+function mostrarAutos() {
+    autos.forEach( auto => {
+        const {marca, modelo, year, puertas, transmision, precio, color} = auto
+        const autoHTML = document.createElement("p");
+
+        autoHTML.textContent = `${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision} Color: ${color}` ;
+
+        //Insertamos en el HTML
+        resultado.appendChild(autoHTML)
+    })
+}
