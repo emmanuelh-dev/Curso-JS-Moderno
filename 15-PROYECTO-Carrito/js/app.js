3fd9bba8c2487a3c165f14ca672f3430870cd1df
Emmanuel Hernandez <86961961+EmmaxPlay@users.noreply.github.com>
2022-03-06 14:23:17 -0600

    Mostrando el carrito en el HTML

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -38,5 +38,47 @@ function leerDatosCurso(curso){
         id: curso.querySelector('a').getAttribute('data-id'),
         cantidad: 1
     }
-    console.log(infoCurso)
+    //Agrega los elementos al carrito
+    articulos = [...articulos, infoCurso]
+
+    console.log(articulos);
+
+    carritoHTML();
+}
+
+//Muestra el carrito de compras en el html
+
+function carritoHTML(){
+
+    //Limpiando el HTML
+    limpiarHTML()
+
+
+    //Recorre el carrito y genera el HTML
+
+    articulos.forEach( curso => {
+        const { imagen, titulo, precio, cantidad, id} = curso;
+        const row = document.createElement('tr');
+        row.innerHTML = 
+        `
+        <td><img src="${imagen}" width="150"/></td>
+        <td>${titulo}</td>
+        <td>${precio}</td>
+        <td>${cantidad}</td>
+
+        <td>    <a href="#" class="borrar-curso" data-id="${id}">X</a></td>
+        `
+
+        //Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    })
+}
+
+
+//Elimina los cursos del tbody
+function limpiarHTML(){
+
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
 }
