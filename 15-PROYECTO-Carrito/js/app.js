84d3fea13a1b617b1704a3a5148f72a2fb1d54c7
Emmanuel Hernandez <86961961+EmmaxPlay@users.noreply.github.com>
2022-03-06 13:10:52 -0600

    Variables

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -1,26 +1,7 @@
-// Variables
+//Variables
 const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const vaciarCarrito = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 
-cargarEventListeners();
-function cargarEventListeners() {
-    //Cuando agregas un curso precionando "Agregar al carrito"
-    listaCursos.addEventListener('click', agregarCurso);
-}
 
-//Funciones
-function agregarCurso(e) {
-    e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')){
-        const cursoSeleccionado = e.target.parentElement.parentElement;
-        console.log('cursoSeleccionado');
-        leerDatosCurso()
-    }
-    
-}
-//Lee el contenido del HTML al que le dimos click y extrae  la información del curso
-function leerDatosCurso(){
-    console.log(curso);
-}
