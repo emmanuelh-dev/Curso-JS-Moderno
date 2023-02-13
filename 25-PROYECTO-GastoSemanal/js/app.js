28e358137428e44667ad77207237be6c39bc9318
emmanuelh-dev <86961961+emmanuelh-dev@users.noreply.github.com>
2023-02-13 08:24:07 -0600

    Se inicia el proyecto de gasto semanal

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -1,121 +1,99 @@
 //Variables y selectores
-const formulario = document.querySelector("#agregar-gasto")
-const gastoListado = document.querySelector("#gasto ul")
+const formulario = document.querySelector("#agregar-gasto");
+const gastoListado = document.querySelector("#gastos ul");
 
-//Events
-eventListeners  ()
+//Eventos
+eventListeners();
 function eventListeners() {
-    document.addEventListener('DOMContentLoaded', preguntarPresupuesto)
-
-    formulario.addEventListener('submit', agregarGasto)
+  document.addEventListener("DOMContentLoaded", preguntarPresupuesto);
+  formulario.addEventListener("submit", agregarGasto);
 }
 
-//classes
+//Clases
 class Presupuesto {
-    constructor(presupuesto){
-        this.presupuesto = Number(presupuesto)
-        this.restante = Number(presupuesto)
-        this.gastos = []
-    }
-
-    nuevoGasto(gasto){
-    this.gasto = [...this.gastos, gasto]
-    console.log(this.gasto)
-    }
-}
-class UI{
-    insertarPresupuesto( cantidad ) {
-        //Extraemos los valores
-        const {presupuesto, restante} = cantidad
-
-        //Add to HTML
-        document.querySelector('#total').textContent = presupuesto
-        document.querySelector('#restante').textContent = restante
-    }
-
-    //Print Error
-    imprimirAlerta(mensaje, tipo){
-        //crate div
-        const divMensaje = document.createElement('div')
-        divMensaje.classList.add("text-center", "alert")
-
-        if (tipo === "error"){
-            divMensaje.classList.add("alert-danger")
-        }else{
-            divMensaje.classList.add("alert-success")
-        }
-
-        //Mensaje error
-        divMensaje.textContent = mensaje
-
-        //Insert
-        document.querySelector(".primario").insertBefore(divMensaje, formulario)
-
-        //Remove
-        setTimeout( () => {
-            divMensaje.remove()
-        },2000)
-    }
-
-    agregarGastoListado(gastos){
-
-
-        //Iterar sobre los gastos
-        gastos.forEach(gasto => {
-            console.log(gasto)
-        });
-    }
+  constructor(presupuesto) {
+    this.presupuesto = Number(presupuesto);
+    this.restante = this.presupuesto;
+    this.gastos = [];
+  }
+  nuevoGasto(gasto) {
+    this.gastos = [...this.gastos, gasto];
+    console.log(this.gastos);
+  }
 }
 
-//Instanciar
-const ui = new UI()
-let presupuesto
+class UI {
+  insertarPresupuesto(cantidad) {
+    //Extraemos el valor
+    const { presupuesto, restante } = cantidad;
+    //Insertamos el presupuesto en el HTML
+    document.querySelector("#total").textContent = presupuesto;
+    document.querySelector("#restante").textContent = restante;
+  }
 
-//Functions
+  imprimirAlerta(mensaje, type) {
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add("text-center", "alert");
+
+    if (type === "error") {
+      divMensaje.classList.add("alert-danger");
+    } else if (type === "success") {
+      divMensaje.classList.add("alert-success");
+    }
+
+    //Mensaje
+    divMensaje.textContent = mensaje;
+
+    //Insertamos el mensaje en el HTML
+    document.querySelector(".primario").insertBefore(divMensaje, formulario);
+    //Eliminamos el mensaje
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
+}
+
+//Instancias
+const ui = new UI();
+let presupuesto;
+//Funciones
+
 function preguntarPresupuesto() {
-    const presupuestoUsuario = prompt("Cual es tu presupuesto?")
+  let presupuestoUsuario = prompt("Ingrese el presupuesto de la semana");
+  presupuestoUsuario = Number(presupuestoUsuario);
+  while (
+    !presupuestoUsuario ||
+    isNaN(presupuestoUsuario) ||
+    presupuestoUsuario <= 0
+  ) {
+    alert("Entrada inválida. Por favor, ingrese un número válido.");
+    presupuestoUsuario = prompt("Ingrese el presupuesto de la semana");
+    presupuestoUsuario = Number(presupuestoUsuario);
+  }
+  //Instanciamos el presupuesto
+  presupuesto = new Presupuesto(presupuestoUsuario);
 
-    if(presupuestoUsuario === "" || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
-        window.location.reload()
-    }
-    presupuesto = new Presupuesto(presupuestoUsuario)
-
-    ui.insertarPresupuesto(presupuesto)
+  ui.insertarPresupuesto(presupuesto);
 }
 
+function agregarGasto(e) {
+  e.preventDefault();
 
-//Add gastos
-function agregarGasto(e){
-    e.preventDefault()
+  //Leemos los datos del formulario
+  const nombre = document.querySelector("#gasto").value;
+  const cantidad = Number(document.querySelector("#cantidad").value);
 
+  //Validamos que el nombre no este vacío
+  if (nombre === "" || cantidad === "") {
+    ui.imprimirAlerta("Ambos campos son obligatorios", "error");
+  } else if (cantidad <= 0 || isNaN(cantidad)) {
+    ui.imprimirAlerta("Debe ingresar un valor valido", "error");
 
-    //Read data formulario
-    const nombre = document.querySelector("#gasto").value
-    const cantidad = Number(document.querySelector("#cantidad").value)
+    return;
+  }
+  const gasto = { nombre, cantidad, id: Date.now() };
 
-    //Validation
-    if(nombre === "" || cantidad === ""){
-        ui.imprimirAlerta("Ambos campos son obligatorios", "error")
-        return
-    } else if(cantidad <= 0 || isNaN(cantidad)){
-        ui.imprimirAlerta("Cantidad no valida", "error")
-        return
-    }
-    
-    //Generar un objeto con el gasto
-    const gasto = { nombre, cantidad, id: Date.now() }
-
-    //Añade un nuevo gasto
-    presupuesto.nuevoGasto(gasto)
-
-    ui.imprimirAlerta("Gasto agregado correctamente", "success")
-
-    //imprimir los gastos
-    const {gastos} = presupuesto
-    ui.agregarGastoListado(presupuesto)
-
-
-    //reinicia el formulario
-    formulario.reset()
-
+  presupuesto.nuevoGasto(gasto);
+  ui.imprimirAlerta("Gasto agregado con éxito", "success");
+  formulario.reset();
 }
